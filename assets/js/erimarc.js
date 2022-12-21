121730f6aa3fa618d6277b0dc7695e295c5430c0
Aldair Cajicua <aldaircajicua@gmail.com>
2022-12-21 22:39:16 +0100

    update script

--- assets/js/erimarc.js
+++ assets/js/erimarc.js
@@ -26,3 +26,40 @@ d.addEventListener('click', function(e){
     );
     e.preventDefault();
 });
+
+function countTo(){
+    let from = -50;
+    let to = 20;
+    let step = to > from ? 1 : -1;
+    let interval = 90;
+
+    if(from == to){
+        document.querySelector("#output").textContent = from;
+        return;
+    }
+
+    let counter = setInterval(function(){
+        from += step;
+        // document.querySelector("#output").textContent = from;
+        document.querySelector('.views-number').textContent = from;
+        document.querySelector('.charging-number').textContent = from;
+        document.querySelector('.recycle-number').innerHTML = from;
+        if(from == to){
+            clearInterval(counter);
+        }
+    }, interval);
+}
+
+let scWidth = window.screen.width;
+let scHeight = window.screen.height;
+let fivePercent = scWidth / 5;
+
+window.addEventListener('scroll', (e)=>{
+    // const rect = document.querySelector('.bg-image-alternate').getBoundingClientRect();
+    const rect = document.querySelector('.bg-image-alternate').get;
+    let scrollTop = window.scrollY;
+    console.log(rect);
+    if ( scrollTop == 822 ){
+        countTo();
+    }
+});
